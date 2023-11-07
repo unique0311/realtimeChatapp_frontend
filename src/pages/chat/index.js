7437e3890237411f80eae074bf6fd4feb729cc5f
fredgeorge0311 <george.fred0311@gmail.com>
2023-11-06 20:58:46 -0500

    completed frontend

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,10 +1,12 @@
 import styles from "./styles.module.css";
 import MessagesReceived from "./messages";
 import SendMessage from "./send-message";
+import RoomAndUsers from "./room-and-users";
 
 const Chat = ({ socket, userName, room }) => {
   return (
     <div className={styles.chatContainer}>
+      <RoomAndUsers socket={socket} userName={userName} room={room} />
       <div>
         <MessagesReceived socket={socket} />
         <SendMessage socket={socket} userName={userName} room={room} />
--- src/pages/chat/messages.js
+++ src/pages/chat/messages.js
@@ -1,8 +1,10 @@
 import styles from "./styles.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Messages = ({ socket }) => {
   const [messageReceived, setMessageReceived] = useState([]);
+
+  const messagesColumnRef = useRef(null);
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
@@ -20,13 +22,35 @@ const Messages = ({ socket }) => {
     return () => socket.off("receive_message");
   }, [socket]);
 
+  useEffect(() => {
+    socket.on("last_100_messages", (last100Messages) => {
+      console.log("Last 100 messages:", JSON.parse(last100Messages));
+      last100Messages = JSON.parse(last100Messages);
+      last100Messages = sortMessagesByDate(last100Messages);
+      setMessageReceived((state) => [...last100Messages, ...state]);
+    });
+
+    return () => socket.off("last_100_messages");
+  }, [socket]);
+
+  useEffect(() => {
+    messagesColumnRef.current.scrollTop =
+      messagesColumnRef.current.scrollHeight;
+  }, [messageReceived]);
+
+  function sortMessagesByDate(messages) {
+    return messages.sort(
+      (a, b) => parseInt(a.__createdtime__) - parseInt(b.__createdtime__)
+    );
+  }
+
   function formatDateFromTimestamp(timestamp) {
     const date = new Date(timestamp);
     return date.toLocaleString();
   }
 
   return (
-    <div className={styles.messagesColumn}>
+    <div className={styles.messagesColumn} ref={messagesColumnRef}>
       {messageReceived.map((msg, i) => (
         <div className={styles.message} key={i}>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
